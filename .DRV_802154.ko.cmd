39232820265c7254ad78a66025f092565c4b4531
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2016-03-08 23:57:57 +0900

    add device.pref

---
+++ .DRV_802154.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/driver/LazDriver/DRV_802154.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/driver/LazDriver/DRV_802154.ko /home/pi/driver/LazDriver/DRV_802154.o /home/pi/driver/LazDriver/DRV_802154.mod.o
---
+++ .DRV_802154.o.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/driver/LazDriver/DRV_802154.o := ld -EL    -r -o /home/pi/driver/LazDriver/DRV_802154.o /home/pi/driver/LazDriver/drv-802154e.o /home/pi/driver/LazDriver/mac-802154e.o /home/pi/driver/LazDriver/phy-bp3596.o /home/pi/driver/LazDriver/i2c-bp3596.o /home/pi/driver/LazDriver/spi-bp3596.o /home/pi/driver/LazDriver/ieee802154e.o 
