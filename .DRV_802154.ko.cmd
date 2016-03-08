7f82e24f455226d899be7d28026ce1efdc88986f
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2016-03-09 00:00:06 +0900

    update .ignore

--- .DRV_802154.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/driver/LazDriver/DRV_802154.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/driver/LazDriver/DRV_802154.ko /home/pi/driver/LazDriver/DRV_802154.o /home/pi/driver/LazDriver/DRV_802154.mod.o
