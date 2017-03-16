7de43487dd2d60ca99ecf8ffa6efe67b95265df1
Eiichi Saito <saito253@dsn.lapis-semi.com>
2017-03-16 17:40:07 +0900

    rm test

--- test/lib/LazGem.rb
+++
@@ -1,6 +0,0 @@
-module LazGem; end
-
-require_relative "LazGem/version"
-require_relative "LazGem/device"
-require_relative "LazGem/ioctl"
-
--- test/lib/LazGem/version.rb
+++
@@ -1,3 +0,0 @@
-module LazGem
-    VERSION = "0.0.8"
-end
