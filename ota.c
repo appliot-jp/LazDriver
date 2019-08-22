bc42ee0efae326d0e764f7e7988b230dc525ab62
YoshiyukiTanaka <tanaka802@dsn.lapis-semi.com>
2019-08-22 16:37:47 +0900

    add some OTA sub functions

--- ota.c
+++ ota.c
@@ -8,7 +8,6 @@
 #include "wdt.h"
 #include "driver_irq.h"
 #include "hwif/wire0.h"
-#include "hwif/hal.h"
 #include "ota.h"
 //#define OTA_DEBUG
 #ifdef OTA_DEBUG
@@ -36,12 +35,18 @@
 static const uint8_t hw_type = OTA_HW_TYPE_MINI;
 
 #pragma SEGCODE "OTA_SEGCODE"
+#pragma SEGINIT "OTA_SEGINIT"
 #pragma SEGNOINIT "OTA_SEGNOINIT"
 #pragma SEGCONST "OTA_SEGCONST"
 
 //
 // API function
 //
+
+void ota_init(void)
+{
+	return;
+}
 
 void ota_boot(void)
 {
@@ -63,9 +68,24 @@ uint8_t ota_get_version(void)
 	return 0;
 }
 
+bool ota_check_aes_key(void)
+{
+	int i;
+	uint8_t cnt00 = 0, cntFF = 0;
+
+	for (i=0; i<OTA_AES_KEY_SIZE; i++) {
+		if (ota_aes_key[i] == 0x00) cnt00++;
+		else if (ota_aes_key[i] == 0xFF) cntFF++;
+	}
+	if ((cnt00 == OTA_AES_KEY_SIZE) || (cntFF == OTA_AES_KEY_SIZE)) return false;	// all 0x00 or 0xFF
+	else return true;
+}
+
 const st_OTA_f OTA = {
+	ota_init,
 	ota_boot,
 	ota_start,
 	ota_get_hw_type,
-	ota_get_version
+	ota_get_version,
+	ota_check_aes_key
 };
